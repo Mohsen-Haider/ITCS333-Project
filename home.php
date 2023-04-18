802ca0b4f3843d84ba6df8799cdfedc3d94f35c0
Mohsen <mohsen00091@gmail.com>
2023-04-18 15:23:48 +0300

    Adding the Home page with its stylesheet file

---
+++ home.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Daniel Scott Branding and Logo Design in Auckland New Zealand</title>
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+        <script src="https://kit.fontawesome.com/01bac212b7.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Asap:400,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css-reset.css">
+        <link rel="stylesheet" href="style.css">
+        
+
+    </head>
+    <body>
+
+    <div class="header-container">
+  <header>
+    <div class="logo"><i class="fa-brands fa-qq"></i><p>QuizLand</p></div>
+    <nav>
+      <form action="" method="post">
+        <input class="search" type="text" placeholder="    Search   ">
+      </form>
+
+
+
+<div class="button-container">
+    <div class="dropdown">
+        <a href="#" class="dropbtn">Home</a>
+    </div>
+
+    <div class="dropdown">
+        <a href="#" class="dropbtn">Profile</a>
+        <div class="dropdown-content">
+            <a href="#">Category 1</a>
+            <a href="#">Category 2</a>
+            <a href="#">Category 3</a>
+        </div>
+    </div>
+
+    <div class="dropdown">
+        <a href="#" class="dropbtn">Categories</a>
+        <div class="dropdown-content">
+            <a href="#">Category 1</a>
+            <a href="#">Category 2</a>
+            <a href="#">Category 3</a>
+        </div>
+    </div>
+
+    <div class="dropdown">
+        <a href="#" class="dropbtn">About</a>
+    </div>
+
+
+</div>
+
+
+
+<html>
+<head>
+    <title>Login Button </title>
+</head>
+<body>
+    <form method="post" action="welcome.php">
+        <input type="submit" value="Log in">
+    </form>
+    
+    
+</body>
+</html>
+      
+
+   
+    </nav>
+  </header>
+</div>
+
+
+        
+        <body>
+
+        <h1>WELCOME TO QUIZLAND</h1>
+     <!-- Define the slideshow container -->
+     <div id="slideshow">
+
+         <div class="slide-wrapper">
+              
+             <!-- Define each of the slides
+          and write the content -->
+             <div class="slide">
+                <img src="new2.jpg" alt="PICTURE">
+             </div>
+             <div class="slide">
+                <img src="image11.png" alt="PICTURE">
+             </div>
+             <div class="slide">
+                <img src="new2.jpg" alt="PICTURE">
+             </div>
+             <div class="slide">
+                 <img src="image11.png" alt="PICTURE">
+             </div>
+         </div>
+     </div>
+ </body>
+
+
+
+
+    
+
+ 
+
+        <div class="container">
+            <div class="MCQ">
+                <p>Mathematics</p>
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image1@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image2@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image3@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image4@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+
+
+        <a href="#">
+             <button class="See">See more</button>
+        </a>
+
+        </div>
+
+        
+
+        <div class="TorF">
+        <p>English and Language Arts</p>
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image5@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image6@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image7@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image8@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+        <a href="#">
+             <button class="See">See more</button>
+        </a>
+
+        </div>
+
+
+
+        <div class="Fill">
+        <p>Science</p>
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image9@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image10@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image5@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+        <a href="http://example.com">
+        <div class="boxing">
+        <img src="images/image9@2x.png" alt="Image">
+        <h2>Title</h2>
+        <p>Description</p>
+        </div>
+        </a>
+
+        <a href="#">
+             <button class="See">See more</button>
+        </a>
+
+        </div>
+
+
+        </div>
+
+
+
+
+
+
+
+
+        
+        <!--
+
+            <div class="col"><img src="images/image2@2x.png" alt="">
+                <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col"><img src="images/image2@2x.png" alt="">
+                <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col"><img src="images/image3@2x.png" alt="">
+                <p>Branding: I'm going to try an impress you with my typing skills.</p>
+            </div>
+            <div class="col"><img src="images/image4@2x.png" alt="">
+                <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col hide-mobile"><img src="images/image5@2x.png" alt="">
+                <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col hide-mobile"><img src="images/image6@2x.png" alt="">
+                <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col hide-mobile"><img src="images/image7@2x.png" alt="">
+                <p>Branding: Lorem  ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col hide-mobile"><img src="images/image8@2x.png" alt="">
+                <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col hide-mobile"><img src="images/image9@2x.png" alt="">
+                <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col hide-desktop-only"><img src="images/image10@2x.png" alt="">
+                <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+
+
+         -->
+
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="crossorigin="anonymous"></script>
+        <script src="scripts.js"></script>
+
+ 
+
+
+
+</body>
+</html>
