168d01bdcec92e08d249b1ea3f32e85ac4217be4
Mohsen <mohsen00091@gmail.com>
2023-04-25 00:24:28 +0300

    Adding burger menu, search bar, signin button, padding to the header and categories and resizing the buttons of the header. we should edit the menu and the categories after the window get smaller using media query.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ITCS333-Project
 This repository contains the online quiz website for the project of the ITCS333 course.
+<h2>
+Here are some possible categories for a quizzes website:
+</h2>
+<p>
+Entertainment: This category could include quizzes about movies, TV shows, music, and celebrities.
+History: Quizzes in this category could cover topics such as world history, American history, and the history of specific cultures or time periods.
+Science: This category could include quizzes about biology, chemistry, physics, astronomy, and other scientific topics.
+Geography: Quizzes in this category could cover countries, cities, landmarks, and other geographical features.
+Sports: This category could include quizzes about individual sports, teams, and famous athletes.
+Pop Culture: Quizzes in this category could cover a range of topics related to current trends and popular culture.
+Literature: Quizzes in this category could cover classic and contemporary books, authors, and literary genres.
+Food and Drink: This category could include quizzes about cooking, cuisine, cocktails, and wine.
+Language: Quizzes in this category could cover grammar, vocabulary, and other aspects of language learning.
+Personality: This category could include quizzes that help users learn more about their own personality traits and characteristics.
+Of course, there are many other possible categories for quizzes, depending on the specific focus of the website and the interests of its users.
+</p>
--- header.php
+++ header.php
@@ -1,17 +1,26 @@
+<!-- /*  temperory comment start */
+ -->
 
-<div class="header-container">
+ <div class="header-container">
   <header>
     <a href="home.php"><div class="logo"><i class="fa-brands fa-qq"></i><p>QuizLand</p></div></a>
-      <form class="search-form" action="" method="post">
-        <input class="search" type="text" placeholder="    Search   ">
-      </form>     
+
+ <div class="wrap">
+   <div class="search">
+      <input type="text" class="searchTerm" placeholder="What are you looking for?">
+      <button type="submit" class="searchButton">
+        <i class="fa fa-search"></i>
+     </button>
+   </div>
+</div>
+
       <nav>     
       <div class="dropdown">
         <a href="home.php">
   <div class="dropbtn">Home</div>
   </a>
 </div>
-<div class="dropdown">
+<div class="dropdown hdr-hidden">
   <div class="dropbtn">Profile</div>
   <div class="dropdown-content">
     <a href="#">Link 1</a>
@@ -19,7 +28,7 @@
     <a href="#">Link 3</a>
   </div>
 </div>
-<div class="dropdown">
+<div class="dropdown hdr-hidden">
   <div class="dropbtn">Categories</div>
   <div class="dropdown-content">
     <a href="#">Link 1</a>
@@ -33,12 +42,51 @@
 </div>
 
 </nav>
-<div class="dropdown">
-  <a href="#">
-  <div class="dropbtn">Signin</div>
+
+<div class="mob-wrapper">
+
+                    <img src="images/icon90.svg" alt="Mobile Menu Icon" style="width: 40px; cursor:pointer;" class="burgermenu">
+
+                    <div class="mob-nav">
+                  <div class="mob-dropdown">
+        <a href="home.php">
+  <div class="mob-dropbtn">Home</div>
   </a>
 </div>
-<i class="fa-solid fa-user"></i>
-  </header>
+<div class="mob-dropdown hdr-hidden">
+  <div class="mob-dropbtn">Profile</div>
+  <div class="mob-dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+<div class="mob-dropdown hdr-hidden">
+  <div class="mob-dropbtn">Categories</div>
+  <div class="mob-dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+
+<div class="mob-dropdown">
+  <div class="mob-dropbtn">About</div>
+</div>
+</div>
+
+ </div>
+
+
+  <a class=" sig" href="#">
+  <i class="fa-solid fa-user"></i>
+  <div>Signin</div>
+  </a>
+
+  
+  </header> 
+
+  <!-- /*  temperory comment end */ -->
+
 </div>
 
